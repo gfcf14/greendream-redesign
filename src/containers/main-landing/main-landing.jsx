dba56805ea80fe7df596aebd4f784c99d87945c4
Carlos <carloscuba014@gmail.com>
2019-06-11 12:35:36 -0400

    GDR-017 create page-container container

--- src/containers/main-landing/main-landing.jsx
+++ src/containers/main-landing/main-landing.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { Flex, Image } from 'rebass';
 import { logoImage } from 'images';
 import { Menu } from 'components';
+import { PageContainer } from 'containers';
 import './main-landing.scss';
 
 export function MainLanding() {
   return (
     <Flex as="section" className="main-landing-rct-component">
-      <Image
-        src={logoImage}
-        className="logo"
-        alt="logo"
-      />
-      <Menu />
+      <Flex as="section" className="main-landing-rct-component__header">
+        <Flex as="section" className="background">
+          <Image
+            src={logoImage}
+            className="logo"
+            alt="logo"
+          />
+          <Menu />
+        </Flex>
+      </Flex>
+      <PageContainer />
     </Flex>
   );
 }
